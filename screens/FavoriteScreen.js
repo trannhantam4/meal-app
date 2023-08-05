a2d7777bb29d11a4f9e681375b736d7d0d154c2e
trannhantam4 <80136734+trannhantam4@users.noreply.github.com>
2023-08-05 15:06:54 +0700

    fav

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,10 +1,10 @@
 import { View, Text } from "react-native";
-import React from "react";
-
+import React, { useContext } from "react";
+import { MEALS } from "../data/dummy-data";
+import { FavoriteContext } from "../store/context/favorite-content";
+import { MealsList } from "../components/MealsList/MealsList";
 export default function FavoriteScreen() {
-  return (
-    <View>
-      <Text>FavoriteScreen</Text>
-    </View>
-  );
+  const favMealCtx = useContext(FavoriteContext);
+  const favMeals = MEALS.filter((meal) => favMealCtx.ids.includes(meal.id));
+  return <MealsList items={favMeals} />;
 }
