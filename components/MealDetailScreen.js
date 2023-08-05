9bcdbabe99085e22ae4f6e5247c328de12e37da1
trannhantam4 <80136734+trannhantam4@users.noreply.github.com>
2023-08-05 12:53:29 +0700

    fav

--- components/MealDetailScreen.js
+++ components/MealDetailScreen.js
@@ -1,9 +1,37 @@
 import { View, Text, Image } from "react-native";
-import React from "react";
+import React, { useContext, useLayoutEffect } from "react";
 import { MEALS } from "../data/dummy-data";
-export default function MealDetailScreen({ route }) {
+import { Ionicons } from "@expo/vector-icons";
+import { FavoriteContext } from "../store/context/favorite-content";
+export default function MealDetailScreen({ route, navigation }) {
+  const favMealsCtx = useContext(FavoriteContext);
   const id = route.params.mealId;
+
+  const mealIsFav = favMealsCtx.ids.includes(id);
   const selectedMeal = MEALS.find((meal) => meal.id === id);
+
+  function changeFavHandler() {
+    if (mealIsFav) {
+      favMealsCtx.removeFavorite(id);
+    } else {
+      favMealsCtx.addFavorite(id);
+    }
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <Ionicons
+            onPress={changeFavHandler}
+            name="heart"
+            color={mealIsFav ? "red" : "white"}
+            size={24}
+          ></Ionicons>
+        );
+      },
+    });
+  });
   return (
     <View>
       <Image
--- screens/MealOverviewScreen.js
+++ screens/MealOverviewScreen.js
@@ -2,7 +2,8 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 import React from "react";
 import { MEALS, CATEGORIES } from "../data/dummy-data";
 import { useEffect, useLayoutEffect } from "react";
-import MealItem from "../components/MealItem";
+import MealItem from "../components/MealsList/MealItem";
+import { MealsList } from "../components/MealsList/MealsList";
 export default function MealOverviewScreen({ route, navigation }) {
   const catID = route.params.categoryID;
   const displayedMeals = MEALS.filter((mealItem) => {
@@ -16,27 +17,7 @@ export default function MealOverviewScreen({ route, navigation }) {
     navigation.setOptions({ title: categoryTitle });
   }, [catID, navigation]);
 
-  function renderMealItem(itemData) {
-    const item = itemData.item;
-    const mealItemProps = {
-      id: item.id,
-      title: item.title,
-      imgURL: item.imageUrl,
-      duration: item.duration,
-      complexity: item.complexity,
-      affordability: item.affordability,
-    };
-    return <MealItem {...mealItemProps} />;
-  }
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item) => item.id}
-        renderItem={renderMealItem}
-      />
-    </View>
-  );
+  return <MealsList items={displayedMeals} />;
 }
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 16 },
